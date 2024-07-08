9a202b084e1e54ec0431d913afac55a9d5136265
Lernobug <gunjanaggarwal12345.com>
2024-07-08 21:05:33 +0530

    authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
 import Image from "next/image";
-
+import GoogleButton from 'react-google-button'
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 export default function Home() {
+  const session=useSession();
+  const router=useRouter();
+  if(session.status=='authenticated')
+  {
+    router.push('/home')
+  }
   return (
-   <div>
-    hello
-   </div>
+   <main className="text-center w-full mx-auto max-w-[1240px] mt-16">
+     <h1 className="text-4xl font-bold"> Auth Page</h1>
+     <GoogleButton  onClick={()=> signIn('google')} classNamemx-auto mt-16/>
+   </main>
   );
 }
