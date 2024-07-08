bf07d6795ab6b7a1c09b68dee5cf04a2453200f4
Sumit <sumitgarg380@gmail.com>
2024-07-08 22:00:38 +0530

    FIle Remove

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,20 +8,43 @@ export default function Home() {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFile(e.target.files?.[0]);
     console.log(file);
-  }
+  };
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="w-96 h-96 flex flex-col gap-8">
         <div className="w-96 text-center h-48 flex flex-col justify-center items-center shadow-lg rounded-lg">
-          <input type="file" id="fileInput" className="hidden" onChange={handleFileChange} />
-          <label htmlFor="fileInput" className="cursor-pointer">
-            <Image src="/add.svg" width={100} height={100} alt="" />
-            Send File
-          </label>
+          {file ? (
+            <div className="flex justify-between gap-4">
+              <p>{file.name}</p>
+              <button
+                onClick={() => setFile(undefined)}
+                className="ml-4 bg-red-500 text-white px-4 rounded-lg"
+              >
+                X
+              </button>
+            </div>
+          ) : 
+          <div>
+            <input
+              type="file"
+              id="fileInput"
+              className="hidden"
+              onChange={handleFileChange}
+            />
+            <label htmlFor="fileInput" className="cursor-pointer">
+              <Image src="/add.svg" width={100} height={100} alt="" />
+              Send File
+            </label>
+          </div>
+            }
         </div>
         <div className="w-96 text-center h-32 flex flex-col items-center justify-center shadow-lg rounded-lg gap-2">
-            Receive File
-            <input type="text" className="border py-1 px-2 w-60 text-sm bg-gray-200" placeholder="Input Key" />
+          Receive File
+          <input
+            type="text"
+            className="border py-1 px-2 w-60 text-sm bg-gray-200"
+            placeholder="Input Key"
+          />
         </div>
       </div>
     </div>
